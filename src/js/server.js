290334ec5c4ca8b26e06e5a0b03fd974698b61e5
Chad Keibler <chad.keibler@aesynt.com>
2016-08-08 16:36:42 -0400

    added deletion of temp uploaded files at start of server, run once to not have to wait for timer. also formatted the file

--- src/js/server.js
+++ src/js/server.js
@@ -1,10 +1,10 @@
-var formidable = require('formidable'),
-	os = require('os'),
-	http = require('http'),
-	util = require('util'),
-	fs = require('fs.extra'),
-	users = require('./users.json'),
-	path = require('path');
+	var formidable = require('formidable'),
+		os = require('os'),
+		http = require('http'),
+		util = require('util'),
+		fs = require('fs.extra'),
+		users = require('./users.json'),
+		path = require('path');
 	
 	var ResponseHandler = require('./js/responseHandler.js');
 	var Authentication = require('./js/authentication.js');
@@ -18,8 +18,8 @@ var formidable = require('formidable'),
 	var httpHandler = require('./js/httpHandler')(router, responseHandler);
 	
 	const PORT = 8888;
-	var minutes = 1;
-	var interval = minutes * 60 * 1000;
+	const MINUTES = 5;
+	var interval = MINUTES * 60 * 1000;
 	var serverAdd = 'http://localhost:' + PORT;
 
 	var deleteTempFiles = function (){
@@ -45,27 +45,30 @@ var formidable = require('formidable'),
 			console.log('deleting file===> ' + deleteMe);
 			fs.unlinkSync(deleteMe);
 		}
-};
+	};
 
-http.createServer(function(req, res) {
+	//Call once to clean temp files instead of waiting for timer, if any
+	deleteTempFiles();
 	
-	setInterval(deleteTempFiles, interval);
-	
-	switch (req.method){
-		case 'GET':
-			httpHandler.handleGetRequest(res, req, contentType);
-			break;
-		case 'POST':
-			httpHandler.handlePostRequest(res, req);
-			break;
-		case 'PUT':
-			httpHandler.handlePutRequest(res, req, contentType);
-			break;
-		case 'DELETE':
-			httpHandler.handleDeleteRequest(res, req, contentType);
-			break;
-		default:
-	}
-}).listen(PORT);
+	http.createServer(function(req, res) {
+		//set timer to delete temporary uploaded files, if any
+		setInterval(deleteTempFiles, interval);
+		
+		switch (req.method){
+			case 'GET':
+				httpHandler.handleGetRequest(res, req, contentType);
+				break;
+			case 'POST':
+				httpHandler.handlePostRequest(res, req);
+				break;
+			case 'PUT':
+				httpHandler.handlePutRequest(res, req, contentType);
+				break;
+			case 'DELETE':
+				httpHandler.handleDeleteRequest(res, req, contentType);
+				break;
+			default:
+		}
+	}).listen(PORT);
 
-console.log('Server running at --> ' + serverAdd + '/\nCTRL+C to shutdown');
+	console.log('Server running at --> ' + serverAdd + '/\nCTRL+C to shutdown');
