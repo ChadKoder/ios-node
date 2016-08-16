9b916927114647811fa69dd2a859d057e202cfaf
Chad Keibler <chad.keibler@aesynt.com>
2016-08-16 06:54:49 -0400

    modified/added styling. Added plugin for app to run in background. code cleanup. Formatting cleanup

--- src/js/controllers/MainCtrl.js
+++ src/js/controllers/MainCtrl.js
@@ -1,85 +1,41 @@
-/*
- * controllers v  (build 20160812_173517_1)
- * 2016
- * Author: Chad Keibler 
- */
-
 //js/controllers/MainCtrl.js
 angular.module('MainCtrl', []).controller('MainCtrl', ['$window', '$http', '$mdToast', '$sce', '$scope',
 	function ($window, $http, $mdToast, $sce, $scope) {
-	var port = ':8888';
-	var vm = this;
-	vm.ipAddress = 'http://192.168.1.109';
-	vm.username = 'Chad';
-	vm.password = 'pass';
-    vm.inProgress = false;
-    vm.selectedMedia = 'image';
-    vm.mediaSelectText = 'Selecting Photos';
-    
-  //  vm.lfNgMdFileInputPhotosTemplate = $sce.trustAsHtml('<lf-ng-md-file-input id="lfNgMdFileInputBtn" ng-disabled="!vm.username || !vm.password" preview multiple accept="image/*" lf-files="vm.files" filecount lf-browse-label="Select Photos"></lf-ng-md-file-input>');
-                                                       
-  //  vm.lfNgMdFileInputVideoTemplate = $sce.trustAsHtml('<lf-ng-md-file-input id="lfNgMdFileInputBtn" ng-disabled="!vm.username || !vm.password" preview accept="video/*" lf-files="vm.files" filecount lf-browse-label="Select Video"></lf-ng-md-file-input>');
-    
-     
-    vm.refresh = function () {
-        if (!$scope.$$phase) {
-            $scope.$apply();
-        }
-    };
-    
-    vm.toggleSelectedMedia = function() {
-        var elButton = document.getElementById('lfNgMdFileInputBtn');
-            if (vm.selectedMedia === 'image') {
-               vm.selectedMedia = 'video';
-               vm.mediaSelectText = 'Selecting Video';
-              // vm.lfNgMdInputModel = $sce.trustAsHtml(lfNgMdFileInputVideoTemplate);
-             elButton.removeAttribute('multiple');
-        } else {
-                                                       
-            vm.selectedMedia = 'image';
-            vm.mediaSelectText = 'Selecting Photos';
-             elButton.setAttribute('multiple', '');
-            // vm.lfNgMdInputModel = $sce.trustAsHtml(lfNgMdFileInputPhotosTemplate);
-            
-        }
-        
-        vm.refresh();
-    };
-    
-    
-    
-   // vm.lfNgMdInputModel = lfNgMdFileInputVideoTemplate;
-	
+		var port = ':8888';
+		var vm = this;
+		vm.ipAddress = '';
+		vm.username = '';
+		vm.password = '';
+		vm.inProgress = false;
+		vm.selectedMedia = 'image';
+
+	vm.mediaSelectText = 'Selecting Photos';
+
+	vm.refresh = function () {
+		if (!$scope.$$phase) {
+			$scope.$apply();
+		}
+	};
+
 	vm.showErrorToast = function (err) {
 		vm.showSimpleToast(err);
 	};
-	
+
 	vm.showSuccessToast = function (msg) {
 		vm.showSimpleToast(msg);
 	};
-    
-    vm.showPreview = function () {
-        var elButton = document.getElementById('lfNgMdFileInputBtn');
-        elButton.setAttribute('preview','');
-    };
-    
- /*   vm.mediaSelectText = function () {
-       // var elButton = document.getElementById('lfNgMdFileInputBtn');
-        if (vm.selectedMedia === 'image') {
-          //  elButton.setAttribute('multiple', '');
-            return 'Select Photos';
-        } else {
-          // elButton.removeAttribute('multiple');
-            return 'Select Video';
-        }
-    };*/
-	
+
+	vm.showPreview = function () {
+		var elButton = document.getElementById('lfNgMdFileInputBtn');
+		elButton.setAttribute('preview','');
+	};
+
 	vm.submit = function (){
 		if (!vm.username || !vm.password) {
 			vm.showErrorToast('username and password are required.');
 			return;
 		}
-		
+
 		var encodedAuth = btoa(vm.username + ':' + vm.password);
 		var formData = new FormData();
 		formData.enctype = "multipart/form-data";
@@ -87,37 +43,38 @@ angular.module('MainCtrl', []).controller('MainCtrl', ['$window', '$http', '$mdT
 		angular.forEach(vm.files, function (obj) {
 			formData.append('file', obj.lfFile);
 		});
-		
+
 		$http.defaults.headers.common.Authorization = 'Basic ' + encodedAuth;
-        vm.inProgress = true;
+		vm.inProgress = true;
+
 		$http.post(vm.ipAddress + port, formData, {
 			transformRequest: angular.identity,
 			headers: { 'Content-Type': undefined }
 		}).then(function(result) {
-            vm.inProgress = false;
+			vm.inProgress = false;
 			vm.showSuccessToast('Album upload successful!');
 		}, function (err) {
-            var errorStatus = err.status.toString().trim();
-            vm.inProgress = false;
+			alert('err: ' + JSON.stringify(err));
+			var errorStatus = err.status.toString().trim();
+			vm.inProgress = false;
 			switch (errorStatus) {
 				case '401':
 					vm.showErrorToast('username/password validate failed.');
 					vm.username = '';
 					vm.password = '';
-                    break;
+					break;
 				case '-1':
 					vm.showErrorToast('Server is not available.');
-                    break;
+					break;
 				default:
 					vm.showErrorToast('Unknown error.');
-                    break;
+					break;
 			}
 		});
 	};
-	
+
 	vm.showSimpleToast = function (msg){
 		$mdToast.showSimple(msg);
 	};
-    
-   //  vm.refresh();
+	
 }]);
--- src/js/functions/fileHandler.js
+++ src/js/functions/fileHandler.js
@@ -3,19 +3,26 @@ function FileHandler(formidable, fileSystem, path, os, responseHandler) {
 		handle: function (res, req){
 			var form = new formidable.IncomingForm();
 			form.parse(req, function(err, fields, files) { 
-				res.writeHead(200, {'content-type': 'text/plain'});
-				res.write('received upload:\n\n');
-				res.end();
+				//res.writeHead(200, {'content-type': 'text/plain'});
+				//res.write('received upload:\n\n');
+				//res.end();
 			});
 		 
 				form.on('progress', function(bytesReceived, bytesExpected) {
-				var percent_complete = (bytesReceived / bytesExpected) * 100;
-			    console.log(percent_complete.toFixed(2));
+				var percentComplete = (bytesReceived / bytesExpected) * 100;
+				process.stdout.write('Progress: ' + percentComplete);
+			    //console.log(percent_complete.toFixed(2));
+			//	res.write(percentComplete.toFixed(2));
+				//console.log('sending progress status...');
+				
+				process.stdout.clearLine();
+				process.stdout.cursorTo(0);
+				process.stdout.write('Upload Progress: ' + percentComplete);
 			});
 		 
 			form.on('error', function(err) {
-				console.log('ERROR!!!!!!!!!');
-				responseHandler.write500InternalError(res, 'An error has occurred during upload');
+				//console.log('ERROR!!!!!!!!!');
+				//responseHandler.write500InternalError(res, 'An error has occurred during upload');
 				return;
 			});
 		 
@@ -39,6 +46,10 @@ function FileHandler(formidable, fileSystem, path, os, responseHandler) {
 							
 						} else {
 							console.log("success!")
+							res.writeHead(200, {'Content-Type': 'Text/Plain' });
+							res.write('Upload Complete.\n\n');
+							res.end();
+						//	responseHandler.write200Success(res, null, null, null);
 						}
 					});
 
